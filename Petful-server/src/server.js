edcef62600578a4b26af8113964377da76eaea2f
Jon <jonathanjackson424@gmail.com>
2020-01-31 13:36:32 -0500

    updated PACKAGEJSON

--- Petful-server/src/server.js
+++ Petful-server/src/server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
 // Catch-all 404
 app.use(function(req, res, next) {
-  const err = new Error("Not Found");
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -17,10 +17,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: app.get("env") === "development" ? err : {}
+    error: app.get('env') === 'development' ? err : {}
   });
 });
 
 app.listen(8080, () => {
-  console.log("Serving on 8080");
+  console.log('Serving on 8080');
 });
