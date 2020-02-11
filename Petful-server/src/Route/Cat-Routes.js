298b4d6709bebada79639658e450075ca5e01166
Jon <jonathanjackson424@gmail.com>
2020-02-10 21:17:55 -0500

    functional

--- Petful-server/src/Route/Cat-Routes.js
+++ Petful-server/src/Route/Cat-Routes.js
@@ -1,17 +1,20 @@
-const express = require("express");
+const express = require('express');
 const catExpress = express.Router();
-const catList = require("../Data/Cat-Data");
+const catList = require('../Data/Cat-Data');
 
-catExpress.get("/", (req, res, next) => {
+catExpress.get('/', (req, res, next) => {
   if (catList.first === null)
-    res.status(404).send("Sorry, no more cats available");
+    res.status(404).json({
+      ok: false,
+      message: 'Sorry, no more cats available'
+    });
   else {
     return res.json(catList.first.value);
   }
 });
 
-catExpress.delete("/remove", (req, res, next) => {
-  return res.status(204).json(catList.dequeue());
+catExpress.delete('/remove', (req, res, next) => {
+  return res.status(200).json(catList.dequeue());
 });
 
 module.exports = catExpress;
