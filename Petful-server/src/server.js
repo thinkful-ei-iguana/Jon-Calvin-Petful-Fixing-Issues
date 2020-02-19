c5c04b0d666da879f8308658a65d9dd51c63d9c9
Jon <jonathanjackson424@gmail.com>
2020-02-18 20:06:53 -0500

    edited package.json to start on npm start

--- Petful-server/src/server.js
+++ Petful-server/src/server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const cors = require("cors");
-const dogExpress = require("./Route/Dog-Routes");
-const catExpress = require("./Route/Cat-Routes");
-const peopleExpress = require("./Route/Person-Route");
-const instantiateLists = require("./Data/Create-Pet-Lists");
+const express = require('express');
+const cors = require('cors');
+const dogExpress = require('./Route/Dog-Routes');
+const catExpress = require('./Route/Cat-Routes');
+const peopleExpress = require('./Route/Person-Route');
+const instantiateLists = require('./Data/Create-Pet-Lists');
 
 // const adoptedPets = instantiateLists queue
 //
@@ -17,28 +17,28 @@ app.use(function(error, req, res, next) {
   res.status(error.status || 500);
   res.json({
     message: error.message,
-    error: app.get("env") === "development" ? error : {}
+    error: app.get('env') === 'development' ? error : {}
   });
 });
 
 //testing to see if this is connected
-app.get("/", (req, res) => {
-  res.send("hi there!");
+app.get('/', (req, res) => {
+  res.send('hi there!');
 });
 
-app.use("/api/dog", dogExpress);
-app.use("/api/cat", catExpress);
-app.use("/api/people", peopleExpress);
+app.use('/api/dog', dogExpress);
+app.use('/api/cat', catExpress);
+app.use('/api/people', peopleExpress);
 
 // Catch-all 404
 app.use(function(req, res, next) {
-  const err = new Error("Not Found");
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 app.listen(8080, () => {
-  console.log("Serving on 8080");
+  console.log('Serving on 8080');
 });
 
 module.exports = app;
