56018b416e1b61267a2f9302e883f402db110e93
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-03-01 12:09:06 -0700

    still hate github

--- Petful-server/src/server.js
+++ Petful-server/src/server.js
@@ -13,17 +13,19 @@ app.use(cors());
 app.use(express());
 
 // Catch-all 500
-app.use(function(error, req, res, next) {
-  res.status(error.status || 500);
-  res.json({
-    message: error.message,
-    error: app.get('env') === 'development' ? error : {}
-  });
-});
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 
 //testing to see if this is connected
 app.get('/', (req, res) => {
-  res.send('hi there!');
+  res.send('Hello, world!');
 });
 
 app.use('/api/dog', dogExpress);
@@ -31,7 +33,7 @@ app.use('/api/cat', catExpress);
 app.use('/api/people', peopleExpress);
 
 // Catch-all 404
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
--- README.md
+++
@@ -1,2 +0,0 @@
-DSA-Petful
-Pet adoption
